5eb5690b5e6b20bd296a56706733a5967dc82ef1
JeongHyunBaek <bjhbhm@gmail.com>
2024-08-04 19:03:03 +0900

    헤더, 푸터, index.tsx, _app_tsx 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  )
 }
+
+export default App;
