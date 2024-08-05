4a5f6ce4c9bffa9628d114e36081895f5e5d6d83
JeongHyunBaek <bjhbhm@gmail.com>
2024-08-05 10:22:54 +0900

    로그인 화면 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,24 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { createTheme, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "react-bootstrap";
+import Head from 'next/head';
+
+const theme = createTheme()
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Head>
+        <title>SkinManager</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </div>
+    </ThemeProvider>
   )
 }
 
